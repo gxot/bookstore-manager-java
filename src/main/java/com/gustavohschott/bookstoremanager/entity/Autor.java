d3aa43d778b6cec9ab0a89a36c309ea66134c61f
gxot <gustschott@gmail.com>
2025-05-26 19:59:45 -0300

    Adição de LOMBOK

---
+++ src/main/java/com/gustavohschott/bookstoremanager/entity/Autor.java
@@ -0,0 +1,27 @@
+package com.gustavohschott.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer idade;
+
+}
---
+++ src/main/java/com/gustavohschott/bookstoremanager/entity/Book.java
@@ -0,0 +1,38 @@
+package com.gustavohschott.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer paginas;
+
+    @Column(nullable = false)
+    private Integer capitulos;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "nome_da_editora", nullable = false, unique = true)
+    private String nomeEditora;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+}
